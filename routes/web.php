024a505d0464cf5a1729cff9afcb09ef9c77523d
Julian <ajarizaga@gmail.com>
2020-09-16 23:44:39 -0300

    rutas

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home',function(){
-    return "esto es home";
+Route::get('/home/{name}',function($name){
+    return view('home',['name'=>$name]);
+    //return "esto es home. Hola ".$name." bienvenido";
 });
